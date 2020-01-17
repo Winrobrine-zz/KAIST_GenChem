29b21c298a4b51648c2a016e954b02e9184333f0
박현우 <ghwhsbsb123@gmail.com>
2020-01-17 20:23:08 +0900

    Redevelop for 2020 Spring.

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,111 @@
-import 'dart:convert';
-
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:gencheminkaist/genchem_home.dart';
-import 'package:gencheminkaist/models/course.dart';
-import 'package:gencheminkaist/models/genchem_theme.dart';
-import 'package:gencheminkaist/pages/loading_page.dart';
-import 'package:gencheminkaist/widgets/genchem.dart';
-import 'package:gencheminkaist/constants/color.dart' as color;
-import 'package:flutter/foundation.dart' as Foundation;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "KAIST GenChem",
-      theme: _buildTheme(),
-      debugShowCheckedModeBanner: false,
-      home: FutureBuilder<RemoteConfig>(
-        future: _setupRemoteConfig(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text("Error: ${snapshot.error}"),
-              ),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.done)
-            return _buildGenChem(snapshot.data);
-          return LoadingPage(backgroundColor: color.PRIMARY_COLOR);
-        },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  GenChem _buildGenChem(RemoteConfig remoteConfig) {
-    return GenChem(
-      courses: (jsonDecode(remoteConfig.getString("courses")) as List<dynamic>)
-          .map((e) => Course.fromMap(e))
-          .toList(),
-      theme: GenChemTheme(
-        primaryColor: color.PRIMARY_COLOR,
-        secondaryColor: color.SECONDARY_COLOR,
-      ),
-      home: GenChemHome(remoteConfig: remoteConfig),
-    );
-  }
-
-  ThemeData _buildTheme() {
-    final base = ThemeData.light();
-    return base.copyWith(
-      appBarTheme: base.appBarTheme.copyWith(color: color.PRIMARY_COLOR),
-      iconTheme: base.iconTheme.copyWith(color: Colors.grey),
-      primaryTextTheme: base.primaryTextTheme.copyWith(
-        title: base.primaryTextTheme.title.copyWith(color: Colors.white),
-      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-Future<RemoteConfig> _setupRemoteConfig() async {
-  final remoteConfig = await RemoteConfig.instance;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  try {
-    if (Foundation.kDebugMode)
-      remoteConfig.setConfigSettings(RemoteConfigSettings(debugMode: true));
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-    remoteConfig.setDefaults(<String, dynamic>{
-      "genchem_url": "http://gencheminkaist.pe.kr/",
-      "notice_url": "http://gencheminkaist.pe.kr/down.htm",
-      "courses": "[]"
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
-    await remoteConfig.fetch(expiration: const Duration(minutes: 15));
-    await remoteConfig.activateFetched();
-  } catch (exception) {
-    print(exception);
   }
 
-  return remoteConfig;
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
--- lib/models/genchem_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GenChemTheme {
-  Color primaryColor;
-  Color secondaryColor;
-
-  GenChemTheme({this.primaryColor, this.secondaryColor});
-}
